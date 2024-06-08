10e4cd8ba02ae6a18ffd3d0ae564665d2c0c0bff
pintukumary <ypintu335@gmail.com>
2024-06-08 15:01:20 +0000

    second commit

---
+++ pintu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Print the current working directory
+echo "Current directory:"
+pwd
+
+# List the contents of the current directory
+echo "Contents of the current directory:"
+ls -al
+
