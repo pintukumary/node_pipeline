9d085b590a5a89b66e3763d7b562f1ebccedb1a3
pintukumary <98653316+pintukumary@users.noreply.github.com>
2026-03-16 17:58:27 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -12,28 +12,28 @@ export NVM_DIR="/root/.nvm"
 nvm install v22
 nvm use v22
 
-export HOME=/root
-export PATH="/root/.nvm/versions/node/v22.21.1/bin:$PATH"
+# export HOME=/root
+# export PATH="/root/.nvm/versions/node/v22.21.1/bin:$PATH"
 
-echo "Node.js version: $(node --version)"
-npm install -g pm2 yarn
+# echo "Node.js version: $(node --version)"
+# npm install -g pm2 yarn
 
-cd /var/www/html
-echo "Node version checking"
-node --version
-npm install
+# cd /var/www/html
+# echo "Node version checking"
+# node --version
+# npm install
 
-if pm2 list | grep -q "arken-frontend-dev"; then
-    echo "🔁 PM2 process 'arken-frontend-dev' found — restarting..."
-    pm2 restart arken-frontend-dev || { echo "❌ PM2 restart failed"; exit 1; }
-else
-    echo "🚀 PM2 process 'arken-frontend-dev' not found — starting new process..."
-    pm2 start npm --name "arken-frontend-dev" -- run dev -- -H 0.0.0.0 -p 3000 || {
-        echo "❌ PM2 start failed. Here is the PM2 list for debugging:"
-        pm2 list
-        exit 1
-    }
-fi
+# if pm2 list | grep -q "arken-frontend-dev"; then
+#     echo "🔁 PM2 process 'arken-frontend-dev' found — restarting..."
+#     pm2 restart arken-frontend-dev || { echo "❌ PM2 restart failed"; exit 1; }
+# else
+#     echo "🚀 PM2 process 'arken-frontend-dev' not found — starting new process..."
+#     pm2 start npm --name "arken-frontend-dev" -- run dev -- -H 0.0.0.0 -p 3000 || {
+#         echo "❌ PM2 start failed. Here is the PM2 list for debugging:"
+#         pm2 list
+#         exit 1
+#     }
+# fi
 
 
 echo "Hello this is start_server.sh file"
